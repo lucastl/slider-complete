90a674c62bd3ed1cc8b70162cf585a6cd432f89c
Lucas Tosello Latini <lucastosellolatini@gmail.com>
2020-12-14 00:57:00 -0300

    basic sliding

--- src/main/Slide/index.js
+++ src/main/Slide/index.js
@@ -1,27 +1,47 @@
 class Slide extends HTMLElement {
+
+    static get observedAttributes() {
+        return ['active'];
+    }
+
     constructor() {
         super();
         this.dom = undefined;
+        this.slide = undefined;
+        this.content = {
+            title: undefined,
+            description: undefined,
+        };
     }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.slide !== undefined) this.isActive();
+    };
+
     connectedCallback() {
-        const title = this.getAttribute('title');
-        const description = this.getAttribute('description');
-        const bgColor = this.getAttribute('bgColor');
+        this.setProps();
+        this.render();
+    };
 
+    setProps = () => {
         this.dom = this.attachShadow({ mode: 'open' });
+        this.content.title = this.getAttribute('title');
+        this.content.description = this.getAttribute('description');
+        this.bgColor = this.getAttribute('bgColor');
+    };
 
-        var style = document.createElement('style');
+    setStyle = () => {
+        const style = document.createElement('style');
         style.textContent = `
             .slide {
                 height: 100%;
                 box-sizing: border-box;
                 margin: 0;
-                width: 100vw;
-                display: flex;
+                width: 100%;
                 justify-content: center;
                 align-items: center;
-                background: ${bgColor}
+                background: ${this.bgColor};
+                display: none;
             }
 
             .content-wrap {
@@ -30,20 +50,41 @@ class Slide extends HTMLElement {
                 flex-direction: column;
                 justify-content: center;
             }
-        `;
 
-        var content = document.createElement('article');
-        content.classList.add('slide');
-        content.innerHTML = `
+            .slide.active {
+                display: flex;
+            }
+        `;
+        this.dom.appendChild(style);
+    };
+
+    setContent = () => {
+        this.slide = document.createElement('article');
+        this.slide.classList.add('slide');
+        this.isActive();
+        const template = `
             <div class="content-wrap">
-                <h2 class="slide-title">${title}</h2>
-                <p class="slide-description">${description}</p>
+                <h2 class="slide-title">${this.content.title}</h2>
+                <p class="slide-description">${this.content.description}</p>
             </div>
         `;
+        this.slide.innerHTML = template;
+        this.dom.appendChild(this.slide);
+    };
 
-        this.dom.appendChild(style);
-        this.dom.appendChild(content);
-    }
+    isActive = () => {
+        const active = this.getAttribute('active')
+        if (active === 'true') {
+            this.slide.classList.add('active');
+        } else {
+            this.slide.classList.remove('active');
+        };
+    };
+
+    render = () => {
+        this.setContent();
+        this.setStyle();
+    };
 
 }
 
--- src/main/Slider/index.js
+++ src/main/Slider/index.js
@@ -1,45 +1,125 @@
 class Slider extends HTMLElement {
+
     constructor() {
         super();
         this.dom = undefined;
+        this.slider = undefined;
+        this.width = undefined;
+        this.height = undefined;
+        this.slidesCount = undefined;
+        this.currentSlide = 0;
+        this.buttons = {
+            back: undefined,
+            next: undefined,
+        };
+
     }
 
     connectedCallback() {
-        const width = this.getAttribute('width');
-        const height = this.getAttribute('height');
-        const slidesCount = this.childElementCount;
+        this.setProps();
+        this.render();
 
+        this.listeners();
+    };
+
+
+    setProps = () => {
         this.dom = this.attachShadow({ mode: 'open' });
+        this.slider = document.createElement('section');
+        this.width = this.getAttribute('width');
+        this.height = this.getAttribute('height');
+        this.slidesCount = this.childElementCount - 1;
+        console.log(this.slidesCount)
+    };
 
-        var style = document.createElement('style');
+    setStyle = () => {
+        const style = document.createElement('style');
         style.textContent = `
             #slider {
-                height: ${height}px;
+                height: ${this.height}px;
                 box-sizing: border-box;
                 overflow: hidden;
-                width: ${width}%;
+                width: ${this.width}%;
+                display: flex;
             }
 
             #slides-wrap {
-                display: flex;
-                height: 100%;
-                width: ${slidesCount}00%;
+                width: 100%;
+            }
+
+            .btn {
+                border: 0;
+                background: rgba(0,0,0,.1);
+                width: 2rem;
+                font-size: 2rem;
+                color: rgba(0,0,0,.2);
+                cursor: pointer;
+            }
+
+            .next {
+                box-shadow: -2px 0 6px rgba(0,0,0,.3);
+                z-index: 1;
+            }
+
+            .back {
+                box-shadow: 2px 0 10px rgba(0,0,0,.3);
+                z-index: 1;
             }
         `;
-
-        this.dom.innerHTML = `
-                <section id="slider">
-                    <div id="slides-wrap">
-                        <slot></slot>
-                    </div>
-                </section>
-            `;
-
         this.dom.appendChild(style);
-    }
+    };
 
+    setContent = () => {
+        const iconNext = this.getAttribute('iconNext');
+        const iconBack = this.getAttribute('iconBack');
+        this.slider.setAttribute('id', 'slider')
+        this.slider.innerHTML = `
+            <button class="btn back">
+                ${iconBack}
+            </button>
+            <div id="slides-wrap">
+                <slot></slot>
+            </div>
+            <button class="btn next">
+                ${iconNext}
+            </button>
+        `;
+        this.dom.appendChild(this.slider);
 
+    };
 
+    listeners = () => {
+        this.buttons.back = this.slider.firstElementChild;
+        this.buttons.next = this.slider.lastElementChild;
+
+        this.buttons.back.addEventListener('click', e => {
+            this.back();
+        });
+        this.buttons.next.addEventListener('click', e => {
+            this.next();
+        });
+    };
+
+    back = () => {
+        Array.from(this.children).forEach(child => {
+            child.setAttribute('active', 'false');
+        });
+        this.currentSlide = this.currentSlide === 0 ? this.slidesCount : this.currentSlide - 1;
+        this.children[this.currentSlide].setAttribute('active', 'true');
+    };
+
+    next = () => {
+        Array.from(this.children).forEach(child => {
+            child.setAttribute('active', 'false');
+        });
+        this.currentSlide = this.currentSlide === this.slidesCount ? 0 : this.currentSlide + 1;
+        this.children[this.currentSlide].setAttribute('active', 'true');
+    };
+
+    render = () => {
+        this.setContent();
+        this.setStyle();
+    };
 
 }
 
